84527fd1ab52158927c308500eac36aeaa3f898b
Usman Mughal <usmanmughal_uog@live.com>
2019-12-21 14:19:45 +0500

    Rename ns-limp-rxswift to limp-rxswift.

--- Pods/Target Support Files/Pods-ns-limp-rxswift/Pods-ns-limp-rxswift-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ns_limp_rxswift : NSObject
-@end
-@implementation PodsDummy_Pods_ns_limp_rxswift
-@end
---
+++ limp-rxswift/limp_rxswift.h
@@ -0,0 +1,19 @@
+//
+//  limp_rxswift.h
+//  limp-rxswift
+//
+//  Created by Arslan Ahmad on 21/12/2019.
+//  Copyright © 2019 Usman Mughal. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for limp_rxswift.
+FOUNDATION_EXPORT double limp_rxswiftVersionNumber;
+
+//! Project version string for limp_rxswift.
+FOUNDATION_EXPORT const unsigned char limp_rxswiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <limp_rxswift/PublicHeader.h>
+
+
